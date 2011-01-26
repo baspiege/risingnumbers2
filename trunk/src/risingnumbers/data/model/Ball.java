4f6f43a9a451195fcb252cf2b505a42737afa923
spiegel_9@hotmail.com <spiegel_9@hotmail.com@ff1b99d2-8be6-049f-7716-4d44b106e9a9>
2011-01-26 03:18:23 +0000

    Add Ball.

---
+++ trunk/src/risingnumbers/data/model/Ball.java
@@ -0,0 +1,36 @@
+package risingnumbers.data.model;
+
+import com.google.appengine.api.datastore.Key; 
+
+import java.io.Serializable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jdo.annotations.IdGeneratorStrategy; 
+import javax.jdo.annotations.IdentityType; 
+import javax.jdo.annotations.PersistenceCapable; 
+import javax.jdo.annotations.Persistent; 
+import javax.jdo.annotations.PrimaryKey; 
+ 
+@PersistenceCapable(identityType = IdentityType.APPLICATION, detachable="true")
+
+/**
+ * Ball which has number and x value.
+ * 
+ * @author Brian Spiegel
+ */
+public class Ball implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+        
+    @PrimaryKey 
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY) 
+    public Key key;
+
+    @Persistent 
+    public int number;
+    
+    @Persistent 
+    public int x;   
+}
